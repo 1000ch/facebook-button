1c3d78568e54f9165fe01ef18bd5fb73c204b763
1000ch <shogosensui@gmail.com>
2021-02-06 17:18:59 +0900

    test: add test

---
+++ test/index.test.ts
@@ -0,0 +1,7 @@
+import { FacebookButton } from '../src/index';
+
+describe('FacebookButton', (): void => {
+  test('should be defined', (): void => {
+    expect(FacebookButton).toBeDefined();
+  });
+});
